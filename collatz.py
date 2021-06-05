3464121a07e6fa591fbec6bc0fea16e1667a307f
SmayanGupta <73956386+SmayanGupta@users.noreply.github.com>
2021-06-05 12:16:41 +0530

    working processing.py code
    This can be used in visualizing collatz conjecture on processing

--- collatz.py
+++ collatz.py
@@ -1,20 +1,67 @@
-import matplotlib.pyplot as plt
-
-
+def setup():
+    size(1200,1200)
+    background(0)
+    
+    #strokeWeight(10)
+    stroke(255,50)
+    fill(255,102,23)
+    
+  
+l=10
 def collatz(n):
-    step=0
-    if n == 1:
-        return 1
+    if n==1:
+        return 0
+    elif n%2==0:
+        return n/2
     else:
+        return(3*n +1)/2
+'''n=500
+seq=[n]
+while n!=1:
+    n=collatz(n)
+    seq.append(n)
+#print(seq)'''
+
+
+
+def draw():
+    angle=0.095#0
+    #put 0.2 for some real cool shit
+    
+    #translate(width/2,height/2)
+    for j in range(2,9000):
+        resetMatrix()
+        translate(0,height/4)#height/2)
+        n=j    
+        #n=50
+        seq=[n]
         while n!=1:
-            if n%2==0:
-                n=n/2
+            n=collatz(n)
+            seq.append(n)
+        for i in range(2,len(seq)+1):
+            #print(seq[-i])
+            if seq[-i]%2==0:
+                rotate(angle)
             else:
-                n=3*n+1
-            step+=1
-            #print(n)
-        return step
-#print(collatz(670617279))
-plt.plot([1,2,3,4])
-plt.axis("off")
-plt.show()
+                rotate(-angle)
+            line(0,0,l,0)
+            translate(l,0)
+                
+                
+                
+
+
+'''
+x2=x1+l*cos(eve)
+                y2=y1+l*sin(eve)
+                line(x1,y1,x2,y2)
+                x1=x2
+                y1=y2
+            else:
+                x2=x1+l*cos(odd)
+                y2=y1+l*sin(odd)
+                line(x1,y1,x2,y2)
+                x1=x2
+                y1=y2
+    
+'''
